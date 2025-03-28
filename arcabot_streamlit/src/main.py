508572da8cf782d446006762b94d3b8e4395277e
s.galli <s.galli@arca24.com>
2025-03-28 14:29:01 +0100

    chromadb code

--- arcabot_streamlit/src/main.py
+++ arcabot_streamlit/src/main.py
@@ -15,12 +15,10 @@ jb_page = st.Page(get_sw_dir("jobarch.py"), title="JOBARCH", icon=":material/sta
 ng_page = st.Page(get_sw_dir("ngage.py"), title="NGAGE",icon=":material/star:")
 tl_page = st.Page(get_sw_dir("talentum.py"), title="TALENTUM", icon=":material/star:")
 jc_page = st.Page(get_sw_dir("jobcourier.py"), title="JOBCOURIER", icon=":material/star:")
-logout_page = st.Page("components/logout.py", title="Log out", icon=":material/logout:")
 
 pg = st.navigation(
    {
        "Software": [jb_page, ng_page, tl_page, jc_page],
-       "Account": [logout_page],
    }
 )
 pg.run()
